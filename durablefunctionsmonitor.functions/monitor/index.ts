8cf11aa9ca4797e232b2c76de1d0d631e032c4c4
Konstantin Lepeshenkov <kolepes@microsoft.com>
2019-09-02 11:46:50 +0200

    simplified a bit the statics hosting code

--- durablefunctionsmonitor.functions/monitor/index.ts
+++ durablefunctionsmonitor.functions/monitor/index.ts
@@ -3,6 +3,10 @@ import { AzureFunction, Context } from '@azure/functions'
 const fs = require('fs');
 const util = require('util');
 const readFileAsync = util.promisify(fs.readFile);
+const fileExistsAsync = util.promisify(fs.exists);
+
+// Root folder where all the statics are copied to
+const wwwroot = './wwwroot';
 
 // A simple statics hosting solution
 const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
@@ -11,73 +15,53 @@ const httpTrigger: AzureFunction = async function (context: Context): Promise<vo
     const path2 = context.bindingData.path2;
     const path3 = context.bindingData.path3;
 
-    if (path1 === 'static') {
-        context.res = await serveStatic(path1, path2, path3);
-        return;
-    }
+    const fileMap = {
+        'static/css': {
+            fileName: `${wwwroot}/static/css/${path3}`,
+            contentType: 'text/css; charset=utf-8'
+        },
+        'static/js': {
+            fileName: `${wwwroot}/static/js/${path3}`,
+            contentType: 'application/javascript; charset=UTF-8'
+        },
+        'static/media': {
+            fileName: `${wwwroot}/static/media/${path3}`,
+            contentType: 'image/svg+xml; charset=UTF-8'
+        },
+        'manifest.json/undefined': {
+            fileName: `${wwwroot}/manifest.json`,
+            contentType: 'application/json; charset=UTF-8'
+        },
+        'favicon.png/undefined': {
+            fileName: `${wwwroot}/favicon.png`,
+            contentType: 'image/png'
+        }
+    };
 
-    if (path1 === 'manifest.json') {
-        context.res = await serveManifestJson();
-        return;
-    }
+    const mapEntry = fileMap[`${path1}/${path2}`];
 
-    if (path1 === 'favicon.png') {
-        context.res = await serveFavicon();
-        return;
-    }
+    if (!!mapEntry) {
 
-    // serving index.html for the rest, to make React Routing work
-    context.res = await serveIndexHtml();
+        if (await fileExistsAsync(mapEntry.fileName)) {
+
+            context.res = {
+                body: await readFileAsync(mapEntry.fileName),
+                headers: { 'Content-Type': mapEntry.contentType }
+            };
+
+        } else {
+
+            context.res = { status: 404 };
+        }
+
+    } else {
+
+        // Returning index.html by default, to support client routing
+        context.res = {
+            body: await readFileAsync(`${wwwroot}/index.html`),
+            headers: { 'Content-Type': 'text/html; charset=UTF-8' }
+        };
+    }
 };
-
-async function serveStatic(path1: string, path2: string, path3: string) {
-    
-    const body = await readFileAsync(`./wwwroot/${path1}/${path2}/${path3}`);
-    
-    var contentType = null;
-    switch (path2) {
-        case 'css':
-            contentType = 'text/css; charset=utf-8';
-            break;
-        case 'js':
-            contentType = 'application/javascript; charset=UTF-8';
-            break;
-        case 'media':
-            contentType = 'image/svg+xml; charset=UTF-8';
-            break;
-    }
-
-    return {
-        body: body,
-        headers: { 'Content-Type': contentType }
-    };
-}
-
-async function serveManifestJson() {
-    return {
-        body: await readFileAsync('./wwwroot/manifest.json'),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        }
-    };
-}
-
-async function serveFavicon() {
-    return {
-        body: await readFileAsync('./wwwroot/favicon.png'),
-        headers: {
-            'Content-Type': 'image/png'
-        }
-    };
-}
-
-async function serveIndexHtml() {
-    return {
-        body: await readFileAsync('./wwwroot/index.html'),
-        headers: {
-            'Content-Type': 'text/html; charset=UTF-8'
-        }
-    };
-}
 
 export default httpTrigger;
