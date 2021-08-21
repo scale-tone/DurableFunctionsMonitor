08d8b2017f33fff6f0b0c3b441a6d07bde015adb
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-08-21 18:09:57 +0200

    refactoring

--- durablefunctionsmonitor.react/src/components/dialogs/ConnectionParamsDialog.tsx
+++ durablefunctionsmonitor.react/src/components/dialogs/ConnectionParamsDialog.tsx
@@ -16,68 +16,65 @@ export class ConnectionParamsDialog extends React.Component<{ state: ConnectionP
     render(): JSX.Element {
         const state = this.props.state;
 
-        return (
-            <Dialog
-                open={state.dialogOpen}
-                onClose={() => state.dialogOpen = false}
-            >
-                <DialogTitle>Manage Storage Connection Settings</DialogTitle>
-                <DialogContent>
+        return (<Dialog open={state.dialogOpen} onClose={() => state.dialogOpen = false}>
+            
+            <DialogTitle>Manage Storage Connection Settings</DialogTitle>
+            <DialogContent>
 
-                    {state.inProgress ? (<LinearProgress />) : (<Box height={4} />)}
-                    
-                    <DialogContentText>
-                        {
-                            state.isReadonly ?
-                                "Change the below values via your application settings ('DFM_HUB_NAME' and 'AzureWebJobsStorage' respectively)" :
-                                "The below values will be saved to local.settings.json file."
-                        }
-                    </DialogContentText>
+                {state.inProgress ? (<LinearProgress />) : (<Box height={4} />)}
+                
+                <DialogContentText>
+                    {
+                        state.isReadonly ?
+                            "Change the below values via your application settings ('DFM_HUB_NAME' and 'AzureWebJobsStorage' respectively)" :
+                            "The below values will be saved to local.settings.json file."
+                    }
+                </DialogContentText>
 
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        label="Hub Name"
-                        fullWidth
-                        disabled={state.inProgress}
-                        InputProps={{ readOnly: true }}
-                        InputLabelProps={{ shrink: true }}
-                        value={state.hubName}
-                        onChange={(evt) => state.hubName = evt.target.value as string}
-                    />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Hub Name"
+                    fullWidth
+                    disabled={state.inProgress}
+                    InputProps={{ readOnly: true }}
+                    InputLabelProps={{ shrink: true }}
+                    value={state.hubName}
+                    onChange={(evt) => state.hubName = evt.target.value as string}
+                />
 
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        label="Azure Storage Connection String"
-                        fullWidth
-                        disabled={state.inProgress}
-                        InputProps={{ readOnly: state.isReadonly }}
-                        InputLabelProps={{ shrink: true }}
-                        value={state.connectionString}
-                        onChange={(evt) => state.connectionString = evt.target.value as string}
-                    />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Azure Storage Connection String"
+                    fullWidth
+                    disabled={state.inProgress}
+                    InputProps={{ readOnly: state.isReadonly }}
+                    InputLabelProps={{ shrink: true }}
+                    value={state.connectionString}
+                    onChange={(evt) => state.connectionString = evt.target.value as string}
+                />
 
-                    <ErrorMessage state={state}/>
+                <ErrorMessage state={state}/>
 
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => state.dialogOpen = false} color="primary">
-                        Cancel
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => state.dialogOpen = false} color="primary">
+                    Cancel
+                </Button>
+
+                {(!state.isReadonly) && (
+                    <Button
+                        onClick={() => state.saveConnectionParams()}
+                        disabled={!state.isDirty || !state.hubName || !state.connectionString || state.inProgress}
+                        color="secondary"
+                    >
+                        Save
                     </Button>
+                )}
 
-                    {(!state.isReadonly) && (
-                        <Button
-                            onClick={() => state.saveConnectionParams()}
-                            disabled={!state.isDirty || !state.hubName || !state.connectionString || state.inProgress}
-                            color="secondary"
-                        >
-                            Save
-                        </Button>
-                    )}
-
-                </DialogActions>
-            </Dialog>
-       );
+            </DialogActions>
+                
+       </Dialog>);
     }
 }
