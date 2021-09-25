d890660803378b8ec1ebf1779938506dbf03be05
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-09-25 16:21:42 +0200

    refactoring

--- durablefunctionsmonitor.dotnetbackend/Functions/Orchestrations.cs
+++ durablefunctionsmonitor.dotnetbackend/Functions/Orchestrations.cs
@@ -16,83 +16,49 @@ using Microsoft.Azure.WebJobs.Extensions.DurableTask.ContextImplementations;
 
 namespace DurableFunctionsMonitor.DotNetBackend
 {
-    public static class StaticOrchestrations
+    public class Orchestrations: HttpHandlerBase
     {
-        public class Orchestrations: HttpHandlerBase
+        public Orchestrations(IDurableClientFactory durableClientFactory): base(durableClientFactory) {}
+
+        // Adds sorting, paging and filtering capabilities around /runtime/webhooks/durabletask/instances endpoint.
+        // GET /a/p/i{connName}-{hubName}/orchestrations?$filter=<filter>&$orderby=<order-by>&$skip=<m>&$top=<n>
+        [FunctionName(nameof(DfmGetOrchestrationsFunction))]
+        public Task<IActionResult> DfmGetOrchestrationsFunction(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Globals.ApiRoutePrefix + "/orchestrations")] HttpRequest req,
+            [DurableClient(TaskHub = Globals.HubNameRouteParamName)] IDurableClient defaultDurableClient,
+            string connName,
+            string hubName,
+            ILogger log)
         {
-            public Orchestrations(IDurableClientFactory durableClientFactory): base(durableClientFactory) {}
+            return this.HandleAuthAndErrors(defaultDurableClient, req, connName, hubName, log, async (durableClient) => {
 
-            // Adds sorting, paging and filtering capabilities around /runtime/webhooks/durabletask/instances endpoint.
-            // GET /a/p/i{connName}-{hubName}/orchestrations?$filter=<filter>&$orderby=<order-by>&$skip=<m>&$top=<n>
-            [FunctionName(nameof(DfmGetOrchestrationsFunction))]
-            public Task<IActionResult> DfmGetOrchestrationsFunction(
-                [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Globals.ApiRoutePrefix + "/orchestrations")] HttpRequest req,
-                [DurableClient(TaskHub = Globals.HubNameRouteParamName)] IDurableClient defaultDurableClient,
-                string connName,
-                string hubName,
-                ILogger log)
-            {
-                return this.HandleAuthAndErrors(defaultDurableClient, req, connName, hubName, log, async (durableClient) => {
+                DateTime? timeFrom, timeTill;
+                string filterString = ExtractTimeRange(req.Query["$filter"], out timeFrom, out timeTill);
 
-                    DateTime? timeFrom, timeTill;
-                    string filterString = ExtractTimeRange(req.Query["$filter"], out timeFrom, out timeTill);
+                string[] statuses;
+                filterString = ExtractRuntimeStatuses(filterString, out statuses);
+                var filterClause = new FilterClause(filterString);
 
-                    string[] statuses;
-                    filterString = ExtractRuntimeStatuses(filterString, out statuses);
-                    var filterClause = new FilterClause(filterString);
+                string hiddenColumnsString = req.Query["hidden-columns"];
+                var hiddenColumns = string.IsNullOrEmpty(hiddenColumnsString) ? new HashSet<string>() : new HashSet<string>(hiddenColumnsString.Split('|'));
 
-                    string hiddenColumnsString = req.Query["hidden-columns"];
-                    var hiddenColumns = string.IsNullOrEmpty(hiddenColumnsString) ? new HashSet<string>() : new HashSet<string>(hiddenColumnsString.Split('|'));
+                // Filtered column should always be returned
+                if(!string.IsNullOrEmpty(filterClause.FieldName))
+                {
+                    hiddenColumns.Remove(filterClause.FieldName);
+                }
 
-                    // Filtered column should always be returned
-                    if(!string.IsNullOrEmpty(filterClause.FieldName))
-                    {
-                        hiddenColumns.Remove(filterClause.FieldName);
-                    }
+                var orchestrations = durableClient
+                    .ListAllInstances(timeFrom, timeTill, !hiddenColumns.Contains("input"), statuses)
+                    .ExpandStatusIfNeeded(durableClient, filterClause, hiddenColumns)
+                    .ApplyRuntimeStatusesFilter(statuses)
+                    .ApplyFilter(filterClause)
+                    .ApplyOrderBy(req.Query)
+                    .ApplySkip(req.Query)
+                    .ApplyTop(req.Query);
 
-                    var orchestrations = durableClient
-                        .ListAllInstances(timeFrom, timeTill, !hiddenColumns.Contains("input"), statuses)
-                        .ExpandStatusIfNeeded(durableClient, filterClause, hiddenColumns)
-                        .ApplyRuntimeStatusesFilter(statuses)
-                        .ApplyFilter(filterClause)
-                        .ApplyOrderBy(req.Query)
-                        .ApplySkip(req.Query)
-                        .ApplyTop(req.Query);
-
-                    return orchestrations.ToJsonContentResult(Globals.FixUndefinedsInJson);
-                });
-            }
-        }
-
-        // Adds 'lastEvent' field to each entity, but only if being filtered by that field
-        private static IEnumerable<ExpandedOrchestrationStatus> ExpandStatusIfNeeded(this IEnumerable<DurableOrchestrationStatus> orchestrations, 
-            IDurableClient client, FilterClause filterClause, HashSet<string> hiddenColumns)
-        {
-            // Only expanding if being filtered by lastEvent
-            if(filterClause.FieldName == "lastEvent") 
-            {
-                return orchestrations.ExpandStatus(client, filterClause, hiddenColumns);
-            } 
-            else
-            {
-                return orchestrations.Select(o => new ExpandedOrchestrationStatus(o, null, hiddenColumns));
-            }
-        }
-
-        // Adds 'lastEvent' field to each entity
-        private static IEnumerable<ExpandedOrchestrationStatus> ExpandStatus(this IEnumerable<DurableOrchestrationStatus> orchestrations,
-            IDurableClient client, FilterClause filterClause, HashSet<string> hiddenColumns)
-        {
-            // Deliberately explicitly enumerating orchestrations here, to trigger all GetStatusAsync tasks in parallel.
-            // If just using yield return, they would be started and finished sequentially, one by one.
-            var list = new List<ExpandedOrchestrationStatus>();
-            foreach (var orchestration in orchestrations)
-            {
-                list.Add(new ExpandedOrchestrationStatus(orchestration,
-                    client.GetStatusAsync(orchestration.InstanceId, true, false, false),
-                    hiddenColumns));
-            }
-            return list;
+                return orchestrations.ToJsonContentResult(Globals.FixUndefinedsInJson);
+            });
         }
 
         // Takes constraints for createdTime field out of $filter clause and returns the remains of it
@@ -146,7 +112,45 @@ namespace DurableFunctionsMonitor.DotNetBackend
             return filterClause;
         }
 
-        private static IEnumerable<ExpandedOrchestrationStatus> ApplyFilter(this IEnumerable<ExpandedOrchestrationStatus> orchestrations,
+        private static readonly Regex RuntimeStatusRegex = new Regex(@"\s*(and\s*)?runtimeStatus in \(([^\)]*)\)(\s*and)?\s*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex TimeFromRegex = new Regex(@"\s*(and\s*)?createdTime ge '([\d-:.T]{19,}Z)'(\s*and)?\s*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex TimeTillRegex = new Regex(@"\s*(and\s*)?createdTime le '([\d-:.T]{19,}Z)'(\s*and)?\s*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+    }
+
+    internal static class ExtensionMethodsForOrchestrations
+    {
+        // Adds 'lastEvent' field to each entity, but only if being filtered by that field
+        internal static IEnumerable<ExpandedOrchestrationStatus> ExpandStatusIfNeeded(this IEnumerable<DurableOrchestrationStatus> orchestrations, 
+            IDurableClient client, FilterClause filterClause, HashSet<string> hiddenColumns)
+        {
+            // Only expanding if being filtered by lastEvent
+            if(filterClause.FieldName == "lastEvent") 
+            {
+                return orchestrations.ExpandStatus(client, filterClause, hiddenColumns);
+            } 
+            else
+            {
+                return orchestrations.Select(o => new ExpandedOrchestrationStatus(o, null, hiddenColumns));
+            }
+        }
+
+        // Adds 'lastEvent' field to each entity
+        internal static IEnumerable<ExpandedOrchestrationStatus> ExpandStatus(this IEnumerable<DurableOrchestrationStatus> orchestrations,
+            IDurableClient client, FilterClause filterClause, HashSet<string> hiddenColumns)
+        {
+            // Deliberately explicitly enumerating orchestrations here, to trigger all GetStatusAsync tasks in parallel.
+            // If just using yield return, they would be started and finished sequentially, one by one.
+            var list = new List<ExpandedOrchestrationStatus>();
+            foreach (var orchestration in orchestrations)
+            {
+                list.Add(new ExpandedOrchestrationStatus(orchestration,
+                    client.GetStatusAsync(orchestration.InstanceId, true, false, false),
+                    hiddenColumns));
+            }
+            return list;
+        }
+
+        internal static IEnumerable<ExpandedOrchestrationStatus> ApplyFilter(this IEnumerable<ExpandedOrchestrationStatus> orchestrations,
             FilterClause filter)
         {
             if(string.IsNullOrEmpty(filter.FieldName))
@@ -184,31 +188,7 @@ namespace DurableFunctionsMonitor.DotNetBackend
             }
         }
 
-        private static IEnumerable<ExpandedOrchestrationStatus> ApplyRuntimeStatusesFilter(this IEnumerable<ExpandedOrchestrationStatus> orchestrations,
-            string[] statuses)
-        {
-            if (statuses == null)
-            {
-                return orchestrations;
-            }
-
-            bool includeDurableEntities = statuses.Contains(DurableEntityRuntimeStatus, StringComparer.OrdinalIgnoreCase);
-            var runtimeStatuses = statuses.ToRuntimeStatuses().ToArray();
-
-            return orchestrations.Where(o => {
-
-                if (o.EntityType == EntityTypeEnum.DurableEntity)
-                {
-                    return includeDurableEntities;
-                }
-                else 
-                {
-                    return runtimeStatuses.Contains(o.RuntimeStatus);
-                }
-            });
-        }
-
-        private static IEnumerable<ExpandedOrchestrationStatus> ApplyOrderBy(this IEnumerable<ExpandedOrchestrationStatus> orchestrations,
+        internal static IEnumerable<ExpandedOrchestrationStatus> ApplyOrderBy(this IEnumerable<ExpandedOrchestrationStatus> orchestrations,
             IQueryCollection query)
         {
             var clause = query["$orderby"];
@@ -224,7 +204,7 @@ namespace DurableFunctionsMonitor.DotNetBackend
         }
 
         // OrderBy that takes property name as a string (instead of an expression)
-        private static IEnumerable<T> OrderBy<T>(this IEnumerable<T> sequence, string fieldName, bool desc)
+        internal static IEnumerable<T> OrderBy<T>(this IEnumerable<T> sequence, string fieldName, bool desc)
         {
             var paramExpression = Expression.Parameter(typeof(T));
             Expression fieldAccessExpression;
@@ -258,7 +238,7 @@ namespace DurableFunctionsMonitor.DotNetBackend
         }
 
         // Helper for formatting orchestration field values
-        private static string GetPropertyValueAsString(this ExpandedOrchestrationStatus orchestration, PropertyInfo propInfo)
+        internal static string GetPropertyValueAsString(this ExpandedOrchestrationStatus orchestration, PropertyInfo propInfo)
         {
             object propValue = propInfo.GetValue(orchestration);
 
@@ -268,12 +248,33 @@ namespace DurableFunctionsMonitor.DotNetBackend
                 propValue.ToString();
         }
 
-        // Some reasonable page size for ListInstancesAsync
-        private const int ListInstancesPageSize = 500;
+        internal static IEnumerable<ExpandedOrchestrationStatus> ApplyRuntimeStatusesFilter(this IEnumerable<ExpandedOrchestrationStatus> orchestrations,
+            string[] statuses)
+        {
+            if (statuses == null)
+            {
+                return orchestrations;
+            }
+
+            bool includeDurableEntities = statuses.Contains(DurableEntityRuntimeStatus, StringComparer.OrdinalIgnoreCase);
+            var runtimeStatuses = statuses.ToRuntimeStatuses().ToArray();
+
+            return orchestrations.Where(o => {
+
+                if (o.EntityType == EntityTypeEnum.DurableEntity)
+                {
+                    return includeDurableEntities;
+                }
+                else 
+                {
+                    return runtimeStatuses.Contains(o.RuntimeStatus);
+                }
+            });
+        }
 
         // Intentionally NOT using async/await here, because we need yield return.
         // The magic is to only load all the pages, when it is really needed (e.g. when sorting is used).
-        private static IEnumerable<DurableOrchestrationStatus> ListAllInstances(this IDurableClient durableClient, DateTime? timeFrom, DateTime? timeTill, bool showInput, string[] statuses)
+        internal static IEnumerable<DurableOrchestrationStatus> ListAllInstances(this IDurableClient durableClient, DateTime? timeFrom, DateTime? timeTill, bool showInput, string[] statuses)
         {
             var queryCondition = new OrchestrationStatusQueryCondition()
             {
@@ -317,6 +318,15 @@ namespace DurableFunctionsMonitor.DotNetBackend
             while (!string.IsNullOrEmpty(response.ContinuationToken));
         }
 
+        // Some reasonable page size for ListInstancesAsync
+        private const int ListInstancesPageSize = 500;
+
+        private const string DurableEntityRuntimeStatus = "DurableEntities";
+
+        private static MethodInfo OrderByMethodInfo = typeof(Enumerable).GetMethods().First(m => m.Name == "OrderBy" && m.GetParameters().Length == 2);
+        private static MethodInfo OrderByDescMethodInfo = typeof(Enumerable).GetMethods().First(m => m.Name == "OrderByDescending" && m.GetParameters().Length == 2);
+        private static MethodInfo ToStringMethodInfo = ((Func<string>)new object().ToString).Method;
+
         private static IEnumerable<OrchestrationRuntimeStatus> ToRuntimeStatuses(this string[] statuses)
         {
             foreach(var s in statuses)
@@ -327,14 +337,5 @@ namespace DurableFunctionsMonitor.DotNetBackend
                 }
             }
         }
-
-        private const string DurableEntityRuntimeStatus = "DurableEntities";
-
-        private static readonly Regex RuntimeStatusRegex = new Regex(@"\s*(and\s*)?runtimeStatus in \(([^\)]*)\)(\s*and)?\s*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-        private static readonly Regex TimeFromRegex = new Regex(@"\s*(and\s*)?createdTime ge '([\d-:.T]{19,}Z)'(\s*and)?\s*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-        private static readonly Regex TimeTillRegex = new Regex(@"\s*(and\s*)?createdTime le '([\d-:.T]{19,}Z)'(\s*and)?\s*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-        private static MethodInfo OrderByMethodInfo = typeof(Enumerable).GetMethods().First(m => m.Name == "OrderBy" && m.GetParameters().Length == 2);
-        private static MethodInfo OrderByDescMethodInfo = typeof(Enumerable).GetMethods().First(m => m.Name == "OrderByDescending" && m.GetParameters().Length == 2);
-        private static MethodInfo ToStringMethodInfo = ((Func<string>)new object().ToString).Method;
     }
 }
